e66260cebe96f355372ded4eb6d674dfd86c2b96
selman-s <gunduzgece546@gmail.com>
2024-06-22 23:42:33 +0300

    Update global CSS and layout
    Add link and mobile menu to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
+import MobileMenu from './MobileMenu'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='h-24 flex items-center justify-between'>
+      {/* LEFT */}
+      <div>
+        <Link href="/" className='font-bold text-xl text-blue-600' >SOCIALBLOG</Link>
+      </div> 
+      {/* CENTER */}
+      <div>
+      </div> 
+      {/* RİGHT */}
+      <div>
+        <MobileMenu />
+      </div>
     </div>
   )
 }
